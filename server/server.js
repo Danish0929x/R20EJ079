a6e190cd371038988275e78f6854a0977b73d01b
Danish0929x <mouzzamdanishh@gmail.com>
2023-06-15 19:18:46 +0530

    new changes

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000; 
+
+
+app.use(bodyParser.json());
+
+app.post('train/register', (req, res) => {
+
+    const { name, email, password } = req.body;
+
+    const response = {
+      message: 'User registered successfully',
+      user: {
+        name,
+        email,
+      },
+    };
+  
+    res.json(response);
+  });
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
